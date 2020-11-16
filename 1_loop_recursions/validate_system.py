4f7c38c58dc707ead19c47e7dc3f477f66ef9f8e
Lukmanova_EV <lev@zaovip.ru>
2020-11-16 21:12:10 +0500

    Separate character_counter in different file

--- 1_loop_recursions/validate_system.py
+++ 1_loop_recursions/validate_system.py
@@ -54,17 +54,7 @@ def test_this_function(function, test_data_directory: Path) -> bool:
         return True
 
 
-def count_characters(string: str) -> str:
-    """ Count characters in given string
 
-    Examples:
-        >>> count_characters('abcdefghijklmnoprstuvwxyz')
-        '25'
-    """
-    counter = 0
-    for char in string:
-        counter += 1
-    return str(counter)
 
 
 if __name__ == '__main__':
